aab2cc121dbd9ef723bbdfd0886d0ffa1b949bcf
YoonJunHyeon <qmdldos@gmail.com>
2024-12-11 15:16:59 +0900

    feat : ThreeScene 컴포넌트 작성
    - 기본틀만 작성 후 테스트

--- time-series/src/component/3D/ThreeScene.js
+++ time-series/src/component/3D/ThreeScene.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function ThreeScene() {
-  return <div>ThreeScene</div>;
+  const mountRef = React.useRef(null);
+
+  useEffect(() => {
+    console.log(" mountRef :", mountRef.current);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        backgroundColor: "gray",
+      }}
+    >
+      relative div
+      <div
+        ref={mountRef}
+        style={{ width: "100%", height: "100vh", outline: "1px solid black" }}
+      >
+        내부 div
+      </div>
+    </div>
+  );
 }
