71c5f85ff3305b9f60acee27a18aee903ea263fc
YoonJunHyeon <qmdldos@gmail.com>
2024-12-19 18:59:49 +0900

    test : Raycaster로 가능한가?
    - 기존 메서드 대신, raycaster로 설정해보기, 계속되는 Raycaster 인식 실패로 다시 시도해봐야할 것 같음

--- time-series/src/component/3D/ThreeScene.jsx
+++ time-series/src/component/3D/ThreeScene.jsx
@@ -1,8 +1,9 @@
-// src/components/3D/ThreeScene.js
+// src/components/3D/ThreeScene.jsx
 
-import React, { useState, useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useState } from "react";
 import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three"; // Raycaster를 직접 사용하려면 필요
 import Cube from "./Cube";
 import Tooltip from "../common/Tooltip";
 
@@ -15,9 +16,8 @@ const ThreeScene = () => {
     text: "",
   });
 
-  // * 큐브 포지션 배열 생성 로직
-  // fiber, 동적으로 큐브 위치 계산
-  const gridSize = 3; // 큐브 크기
+  // 큐브 포지션 배열 생성 로직
+  const gridSize = 3; // 그리드 크기
   const spacing = 1.4; // 큐브 간 간격
 
   const cubes = [];
@@ -26,73 +26,73 @@ const ThreeScene = () => {
       cubes.push({
         position: [(x - 1) * spacing, 0, (z - 1) * spacing],
         color: "green",
-        temperature: 25, // 초기 온도 값 (가데이터 사용 예정)
-        regionCode: "68_10X", // 초기 지역 코드 값 (가데이터 사용 예정)
+        temperature: 25, // 초기 온도 값
+        regionCode: `68_1${x}${z}`, // 초기 지역 코드
       });
     }
   }
 
+  const handlePointerMove = (event) => {
+    const mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    console.log("mouse:", mouse); // {x: -1~1, y: -1~1}
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, event.camera);
+    const intersects = raycaster.intersectObjects(event.scene.children, true);
+
+    console.log("intersects:", intersects);
+
+    if (intersects.length > 0) {
+      const intersectedObject = intersects[0].object;
+      if (intersectedObject.userData.temperature !== undefined) {
+        setTooltip({
+          visible: true,
+          x: event.clientX,
+          y: event.clientY,
+          text: `온도: 섭씨 ${intersectedObject.userData.temperature}도`,
+        });
+      }
+    } else {
+      setTooltip({
+        visible: false,
+        x: 0,
+        y: 0,
+        text: "",
+      });
+    }
+  };
+
   // useEffect(() => {
-  //   console.log("큐브 : ", cubes);
-  // });
-  // useEffect(() => {
-  //   console.log("툴팁 : ", tooltip);
+  //   console.log("툴팁 상태 변경:", tooltip);
   // }, [tooltip]);
 
   return (
     <>
       <Canvas
         style={{ width: "100%", height: "100%" }}
-        onPointerMove={(event) => {
-          // console.log("pointer moved: ", e);
-          const { object } = event;
-          // console.log("object : ", object);
-          // console.log(
-          //   "object.userData.temperature : ",
-          //   object.userData.temperature
-          // );
-          if (object && object.userData.temperature !== undefined) {
-            setTooltip({
-              visible: true,
-              x: event.clientX,
-              y: event.clientY,
-              text: `온도: 섭씨 ${object.userData.temperature}도`,
-            });
-          } else {
-            // setTooltip({
-            //   visible: false,
-            //   x: 0,
-            //   y: 0,
-            //   text: "",
-            // });
-            // console.log("툴팁 false : ", tooltip);
-          }
-        }}
+        camera={{ position: [0, 5, 10], fov: 60 }}
+        onPointerMove={handlePointerMove}
       >
         {/* 카메라 컨트롤 (OrbitControls) */}
         <OrbitControls />
-        {/* 카메라 위치 설정 */}
-        {/* <perspectiveCamera position={[0, 0, 0]} /> */}
-
-        {/* 기본 조명 */}
+        {/* 헬퍼 추가 */}
+        {/* <AxesHelper args={[5]} />
+        <GridHelper args={[10, 10]} /> */}
+        {/* 조명 설정 */}
         <ambientLight intensity={1} />
         <pointLight position={[-10, 10, 10]} />
-        {/* 추가 조명 */}
         <directionalLight position={[5, 5, 5]} intensity={3} />
 
-        {/* 기본 큐브 */}
-        {/* <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="green" />
-      </mesh> */}
-
+        {/* 큐브 렌더링 */}
         {cubes.map((cube, index) => (
           <Cube
             key={index}
             position={cube.position}
             color={cube.color}
-            temperature={25} // 초기 온도 값 (가데이터 사용 예정)
-            regionCode={cube.regionCode} // 초기 지역 코드 값 (가데이터 사용 예정)
+            temperature={cube.temperature}
+            regionCode={cube.regionCode}
           />
         ))}
       </Canvas>
